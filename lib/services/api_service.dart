e656a0fe9ae5f0613d4ece5cb3d15c7b05af2abd
ziadezat <ziad.esam.ezat@gmail.com>
2024-10-07 17:10:33 +0300

    Initial commit

---
+++ lib/services/api_service.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+
+class apiService {
+  Future<String> FetchData() async {
+    // send http request to desired ip from user's input
+    return '0';
+  }
+}
